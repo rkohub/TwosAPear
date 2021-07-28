66bdecedde7af742d0c4c225b1988b89ee930620
rkohub <ryangithub@gmail.com>
2021-07-27 22:27:27 -0500

    Made Sample Stage and Movement
    Made Sample Stage and Movement

--- Two's A Pear/Assets/Scripts/MoveCharacter.cs
+++ Two's A Pear/Assets/Scripts/MoveCharacter.cs
@@ -6,13 +6,16 @@ public class MoveCharacter : MonoBehaviour{
     // Start is called before the first frame update
 
     //Todo
-    //Max Speed
-    //X Ammount of Jumps
+    //Fix Error where reset jumps on hit bottom of platforms
 
     public float jumpMagnitude;
     private Rigidbody2D body;
-    private float xSpeedCap;
-    public float accelerationMagnitude;
+    private float accelerationMagnitude;
+    public bool coll;
+    public int maxJumps;
+    private int jumpsUsed;
+    private float maxVelocity;
+    private Vector2 force;
 
     void Start(){
         body = this.gameObject.GetComponent<Rigidbody2D>();
@@ -20,18 +23,47 @@ public class MoveCharacter : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
+        if(coll){
+            accelerationMagnitude = 1.5f;
+            maxVelocity = 3;
+        }else{
+            accelerationMagnitude = 1;
+            maxVelocity = 2;
+        }
+        if(Mathf.Abs(body.velocity[0]) > maxVelocity){
+            body.velocity = new Vector2 (Mathf.Sign(body.velocity[0]) * maxVelocity,body.velocity[1]);
+        }
+        
         if (Input.GetKey("a")){
             body.AddForce(Vector2.left * accelerationMagnitude);
         }
         if (Input.GetKey("d")){
             body.AddForce(Vector2.right * accelerationMagnitude);
         }
-        if (Input.GetKeyDown("space")){
+        if (Input.GetKeyDown("space") && jumpsUsed < maxJumps){
             jump();
         }
+
+         // print(body.velocity);
     }
 
     public void jump(){
+       body.velocity = new Vector2 (body.velocity[0],0);
        body.AddForce(Vector2.up * jumpMagnitude);
+       jumpsUsed += 1;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Stage"){
+            coll = true;
+            jumpsUsed = 0;
+        }
+        
+    }
+
+    void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.tag == "Stage"){
+            coll = false;
+        }
     }
 }
